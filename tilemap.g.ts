3974fe786c587cba5f5baadaf24f1c0cdc0c3a25
NAAAAAAAAA1212 <96600575+NAAAAAAAAA1212@users.noreply.github.com>
2023-04-16 20:51:20 +0800

    MakeCode 專案的初始檔案

---
+++ tilemap.g.ts
@@ -0,0 +1,14 @@
+// 自動產生的程式碼。請勿編輯。
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency18 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency18":return transparency18;
+        }
+        return null;
+    })
+
+}
+// 自動產生的程式碼。請勿編輯。
